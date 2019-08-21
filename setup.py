7d39d1173aeaa71458643288432908dda656269e
b2220333 <b2220333@gmail.com>
2019-08-21 16:36:50 +0800

    v0.7

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name='mrcnn',
+setup(name='sam_mask_rcnn',
       version='0.1',
-      description='mrcnn',
+      description='sam_mask_rcnn',
       author='Sam Lin',
       author_email='hclin@ira.ee.ntu.edu.tw',
       packages=find_packages()
